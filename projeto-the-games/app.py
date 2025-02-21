a5fd160f2aaf7a6bb6e867425192347dc95b0de3
jvabarroso <jvabarroso@gmail.com>
2025-02-21 11:38:32 -0300

    python no html

--- projeto-the-games/app.py
+++ projeto-the-games/app.py
@@ -1,23 +1,31 @@
 # Comentário em Python
 # Importando o pacote do Flask
-from flask import Flask
+from flask import Flask, render_template
+# Render template é o pacote responsável por carregar as páginas
 
 # Carregando o Flask na variável app
-app = Flask(__name__) # O valor da variável "name" sempre é o nome do arquivo
+app = Flask(__name__, template_folder='views') # O valor da variável "name" sempre é o nome do arquivo. Template folder mostra que todas as páginas estão dentro da View
 
 # Criando a rota principal do site:
 @app.route('/')
 
-
-
-
 # Criando função no Python. Equivalente ao "function" em javascript
+
+# View function - função de visualização. Irá te enviar para a página
 def home():
-    return '<h1>O primeiro de muitos sites Flask. Bem vindo!<h1>'
+    return render_template('index.html')
 
 @app.route('/games')
+# View function - função de visualização. Irá te enviar para a página
 def games():
-    return '<h1>Seja bem vindo a página de games<h1>'
+    titulo = "CS-GO"
+    ano = 2012
+    categoria = "FPS Online"
+    
+    jogadores = ['iruah', 'davi_lambari', 'edsongf', 'kioto', 'black.butterfly', 'jujudopix']
+    return render_template('games.html', titulo=titulo, ano=ano, categoria=categoria, jogadores=jogadores)
+# A primeira variável é a que será chamada na página, enquanto a segunda é o nome da variável em que estão armazenados os dados
+
 
 # Ao inciar o arquivo, o nome dele muda para main, por isso conferir para o arquivo só ser executado se o nome do arquivo for igual a main
 if __name__ == '__main__':
