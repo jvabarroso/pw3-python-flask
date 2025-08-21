a8ee1609158e5e26c042fd136ac59b791eb52632
João Vitor Alves Barroso <jvabarroso@gmail.com>
2025-08-20 22:11:53 -0300

    @jvabarroso

--- Ex-consumo-api/controllers/routes.py
+++ Ex-consumo-api/controllers/routes.py
@@ -1,46 +1,75 @@
 import urllib.request
-from flask import render_template, request, url_for
 import json
-
-
+from flask import render_template
 
 def init_app(app):
 
     @app.route('/')
-    def index():
-        return render_template('index.html')
+    def home():
+        return ongs()
 
-    @app.route('/frases', methods=['GET', 'POST'])
-    @app.route('/frases/<int:id>', methods=['GET', 'POST'])
-    def frases(id=None):
+    @app.route('/ongs')
+    def ongs():
         try:
-            url = 'https://api.quotable.io/random'
-            response = urllib.request.urlopen(url)
-            apiData = response.read()
-            frasesList = json.loads(apiData)
+            url = 'https://ongs-brasil.org/api/ongs'
+            resp = urllib.request.urlopen(url, timeout=10)
+            data = json.loads(resp.read())
+
+            previews = []
+            for ong in data.get('data', []):
+                previews.append({
+                    'id': ong['id'],
+                    'nome': ong['name'],
+                    'cidade': ong.get('city', ''),
+                    'estado': ong.get('state', '')
+                })
+
+            return render_template('index.html', previews=previews)
             
-            # Se id existir (ou seja, foi informado parâmetro)
-            if id:
-                frase = []
-                for f in frasesList:
-                    if f['id'] == id:
-                        frase = f
-                        break
-                if frase:
-                    return render_template('filosofia.html', frase=frase)
-                else:
-                    return f'Frase com a ID {id} não foi encontrada.'
-            else:
-                # Para a rota /frases sem ID, pegar apenas uma frase aleatória
-                frase = {
-                    'texto': frasesList.get('content', 'Frase não disponível'),
-                    'autor': frasesList.get('author', 'Autor desconhecido')
+        except Exception as e:
+            # Se falhar, usar dados de exemplo
+            previews = [
+                {'id': 1, 'nome': 'ONG Exemplo 1', 'cidade': 'São Paulo', 'estado': 'SP'},
+                {'id': 2, 'nome': 'ONG Exemplo 2', 'cidade': 'Rio de Janeiro', 'estado': 'RJ'},
+                {'id': 3, 'nome': 'ONG Exemplo 3', 'cidade': 'Belo Horizonte', 'estado': 'MG'}
+            ]
+            return render_template('index.html', previews=previews)
+
+    @app.route('/ongs/<id>')
+    def ong_detail(id):
+        try:
+            url = f'https://ongs-brasil.org/api/ongs?id={id}'
+            resp = urllib.request.urlopen(url, timeout=10)
+            data = json.loads(resp.read())
+            matches = data.get('data', [])
+            
+            if matches:
+                ong = matches[0]
+                detail = {
+                    'nome': ong.get('name'),
+                    'descricao': ong.get('description'),
+                    'website': ong.get('website'),
+                    'cidade': ong.get('city'),
+                    'estado': ong.get('state')
                 }
-                return render_template('filosofia.html', frase=frase)
-        except:
-            # Em caso de erro, retornar uma frase padrão
-            frase = {
-                'texto': 'A sabedoria começa na admiração.',
-                'autor': 'Sócrates'
+            else:
+                detail = {
+                    'nome': 'Não encontrada',
+                    'descricao': '',
+                    'website': '',
+                    'cidade': '',
+                    'estado': ''
+                }
+
+            return render_template('info.html', detail=detail)
+            
+        except Exception as e:
+            # Se falhar, usar dados de exemplo
+            detail = {
+                'nome': 'ONG de Exemplo',
+                'descricao': 'Esta é uma ONG de exemplo para demonstração.',
+                'website': 'https://exemplo.org',
+                'cidade': 'São Paulo',
+                'estado': 'SP'
             }
-            return render_template('filosofia.html', frase=frase)
+            return render_template('info.html', detail=detail)
